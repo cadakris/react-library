ff027177bb9b15459b92c13dd070cad932af7ced
Kristen <kriscadacio@gmail.com>
2023-11-21 15:58:17 -0500

    changed welcome info

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to fullstack course</h1>
+      <h1>Welcome to fullstack coussrse</h1>
     </div>
   );
 }
