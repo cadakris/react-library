bc1e84ea3ba5a5de5f1151ab5715ff5edab81076
Kristen <kriscadacio@gmail.com>
2024-03-26 08:52:13 -0400

    Included Link and NavLinks to anchor / button tags

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { ReturnBooks } from "./ReturnBooks";
 import BookModel from "../../../models/BookModel";
 import { SpinnerLoading } from "../../utils/SpinnerLoading";
+import { Link } from "react-router-dom";
 
 export const Carousel = () => {
 
@@ -114,7 +115,7 @@ export const Carousel = () => {
                     </div>
 
                     <div className='homepage-carousel-title mt-3'>
-                        <a className="btn btn-outline-secondary btn-lg" href="#">View More</a>
+                        <Link className="btn btn-outline-secondary btn-lg" to="/search">View More</Link>
                     </div>
 
                 </div>
